b432e0b2e8eda8a6e42a60eabc20ee1d54b4f4a6
ahuarte47 <ahuarte47@yahoo.es>
2025-10-29 09:36:24 +0100

    Remove OpenSSL function sample

--- src/pdal_extension.cpp
+++ src/pdal_extension.cpp
@@ -6,9 +6,6 @@
 #include "duckdb/function/scalar_function.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
-
 namespace duckdb {
 
 inline void PdalScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
@@ -18,23 +15,10 @@ inline void PdalScalarFun(DataChunk &args, ExpressionState &state, Vector &resul
 	});
 }
 
-inline void PdalOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Pdal " + name.GetString() + ", my linked OpenSSL version is " +
-		                                           OPENSSL_VERSION_TEXT);
-	});
-}
-
 static void LoadInternal(ExtensionLoader &loader) {
 	// Register a scalar function
 	auto pdal_scalar_function = ScalarFunction("pdal", {LogicalType::VARCHAR}, LogicalType::VARCHAR, PdalScalarFun);
 	loader.RegisterFunction(pdal_scalar_function);
-
-	// Register another scalar function
-	auto pdal_openssl_version_scalar_function = ScalarFunction("pdal_openssl_version", {LogicalType::VARCHAR},
-	                                                            LogicalType::VARCHAR, PdalOpenSSLVersionScalarFun);
-	loader.RegisterFunction(pdal_openssl_version_scalar_function);
 }
 
 void PdalExtension::Load(ExtensionLoader &loader) {
